39652dcfef6c5423ed8b9eb08b61fcbf9a479615
eshankmaheshwari-star <eshankmaheshwari@gmail.com>
2026-08-28 11:09:59 +0000

    utils code

---
+++ src/utils/apiresponse.js
@@ -0,0 +1,19 @@
+class apiResponse{
+    constructor(
+        statusCode,
+        data,
+        message="success",
+    ){
+        this.statusCode=statusCode
+        this.data=data
+        this.message=message
+        this.success=statusCode<400
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export { apiError }
