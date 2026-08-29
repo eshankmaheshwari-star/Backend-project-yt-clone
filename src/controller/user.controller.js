6ea73b35129e88ca89a80c1499b58d69bb150ba0
eshankmaheshwari-star <eshankmaheshwari@gmail.com>
2026-08-29 11:23:03 +0000

    done for user register 1

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,9 +2,20 @@ import { asyncHandler } from "../utils/asynchandler.js"
 console.log("USER ROUTER LOADED");
 
 const registerUser=asyncHandler( async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    });
+    // res.status(200).json({
+    //     message:"ok"
+    // });
+
+    //get user deatils from frontend 
+    // validation-not empty
+    //check if user exists or not:username or email
+    //check for images,avatar
+    //upload them on multer-->cloudinary ,avatar check
+    //create user object -- create entry in db
+    //in response remove refresh token and password
+    //response check for user creation 
+    //return response or error
+
 });
 
 export { registerUser }
